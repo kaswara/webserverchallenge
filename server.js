1a11408fb35e1983a7c122ebec78f7f2f06cefed
kaswara <kk000111@gmail.com>
2019-07-23 23:06:23 +0200

    small silly changes

--- server.js
+++ server.js
@@ -3,9 +3,10 @@
 var fs = require('fs');
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
+
 // Get routes
 var routes = require('./routes/routes.js')(app, fs);
+
 // Launch the server 
 var port = process.env.PORT;
 if (port == null || port == "") {
